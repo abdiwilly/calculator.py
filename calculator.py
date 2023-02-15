11d262c368b19df47f8f1e4fe37dafb308a06c7a
Abdiwilly <abdiwillyhassan@gmail.com>
2023-02-15 11:40:47 +0300

    Hello world

--- calculator.py
+++ calculator.py
@@ -1,23 +1,32 @@
 # this is a calculator pro
 
-number1=eval(input("Enter first number"))
-number2=eval(input("Enter second number"))
+num1=eval(input("Enter first number:"))
+num2=eval(input("Enter second number:"))
 
 operator= input("enter operator:")
 def add(num1,num2):
     result=num1 + num2
-    return result
+    print(result)
 
 def subtract(num1,num2):
     result=num1 - num2
-    return result
+    print(result)
 
 def multiply(num1,num2):
     result=num1 * num2
-    return result
+    print(result)
 
 def divide(num1,num2):
     result=num1 / num2
-    return result
+    print(result)
 
-
+if operator =="+":
+    add(num1,num2)
+elif operator=="-":
+    subtract(num1,num2)
+elif operator=="*":
+    multiply(num1,num2)
+elif operator=="/":
+    divide(num1,num2)
+elif operator=="x" or operator=="x":
+    multiply(num1,num2)
